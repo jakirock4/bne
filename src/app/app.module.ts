4cecb145309114959ce399405b8fabc96c60bac0
Alexis Jair López Gutiérrez <jakirock4@hotmail.com>
2021-10-28 13:59:48 -0500

    fix: 🐛 Se agrega la ruta base para PRDO
    Se agrega la ruta base para que pueda cargar el proyecto en producción

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule],
-    providers: [],
+    providers: [{ provide: APP_BASE_HREF, useValue: '' }],
     bootstrap: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
